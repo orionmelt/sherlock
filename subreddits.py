9bc76056efd1730d2dc0dc0fdd2fdff1d6a2f0ad
Raj <orionmelt@gmail.com>
2014-09-20 19:47:27 -0700

    Added default subreddit indicator
    Also, derive ignore_comments_subs from data instead  of hardcoded list.

--- subreddits.py
+++ subreddits.py
@@ -2,17 +2,20 @@
 
 import csv
 
-ignore_subs = ['writingprompts','jokes','nosleep','photoshopbattles','circlejerk','adviceanimals']
 subreddits=[]
 with open("subreddits.csv") as subreddits_file:
-	for rank, name, subscribers, interest, i1, i2, i3, autotagged, ignore_interest, ignore_comments, attribute, value in csv.reader(subreddits_file, delimiter=',', quoting=csv.QUOTE_NONE):
+	for rank, name, subscribers, interest, i1, i2, i3, default, autotagged, ignore_interest, ignore_comments, attribute, value in csv.reader(subreddits_file, delimiter=',', quoting=csv.QUOTE_NONE):
 		subreddit = {
 			"name": name.lower(),
 			"i1":i1,
 			"i2":i2,
 			"i3":i3,
+			"default":default,
 			"ignore_interest":ignore_interest,
+			"ignore_comments":ignore_comments,
 			"attribute":attribute.lower(),
 			"value":value.lower()
 		}
 		subreddits.append(subreddit)
+ignore_comments_subs 	= [s["name"] for s in subreddits if s["ignore_comments"]=="Y"]
+default_subs 			= [s["name"] for s in subreddits if s["default"]=="Y"]
