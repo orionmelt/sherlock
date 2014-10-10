042b4179c4ae69676b9061542a45ee27e92cd15a
Raj <orionmelt@gmail.com>
2014-10-09 23:41:10 -0700

    Moved CSV file to external location

--- subreddits.py
+++ subreddits.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import csv
+import csv, urllib2
+
+file_url = "https://www.dropbox.com/s/82c1ex12h1njtpv/subreddits.csv?dl=1"
 
 subreddits=[]
-with open("subreddits.csv") as subreddits_file:
-	for rank, name, subscribers, interest, i1, i2, i3, default, autotagged, ignore_interest, ignore_comments, attribute, value in csv.reader(subreddits_file, delimiter=',', quoting=csv.QUOTE_NONE):
-		subreddit = {
-			"name": name.lower(),
-			"i1":i1,
-			"i2":i2,
-			"i3":i3,
-			"default":default,
-			"ignore_interest":ignore_interest,
-			"ignore_comments":ignore_comments,
-			"attribute":attribute.lower(),
-			"value":value.lower()
-		}
-		subreddits.append(subreddit)
+subreddits_file = urllib2.urlopen(file_url)
+#with urllib2.urlopen(file_url) as subreddits_file:
+#with open("subreddits.csv") as subreddits_file:
+for rank, name, subscribers, interest, i1, i2, i3, default, autotagged, ignore_interest, ignore_comments, attribute, value \
+	in csv.reader(subreddits_file, delimiter=',', quoting=csv.QUOTE_NONE):
+	subreddit = {
+		"name": name.lower(),
+		"i1":i1,
+		"i2":i2,
+		"i3":i3,
+		"default":default,
+		"ignore_interest":ignore_interest,
+		"ignore_comments":ignore_comments,
+		"attribute":attribute.lower(),
+		"value":value.lower()
+	}
+	subreddits.append(subreddit)
 ignore_comments_subs 	= [s["name"] for s in subreddits if s["ignore_comments"]=="Y"]
 default_subs 			= [s["name"] for s in subreddits if s["default"]=="Y"]
